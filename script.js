069ab61615405e4958636d4c4b45256f2cc72d88
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-27 17:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+//your JS code here. If requhttps://www.svgrepo.com/show/345221/three-dots.svgired.
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+functionName();
